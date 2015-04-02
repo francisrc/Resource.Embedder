09f9b1575f07ab5143f79b2e261990c3827a89d1
MarcStan <marcstan@live.com>
2015-04-03 00:08:51 +0200

    Initial commit

---
+++ src/AssemblyInfo/AssemblyInfoInc.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © Marc Stanglmayr 2015")]
---
+++ src/ResourceEmbedder/EmbedderTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ResourceEmbedder
+{
+	/// <summary>
+	/// Task to embed files into an existing .Net assembly.
+	/// </summary>
+	public class EmbedderTask : Microsoft.Build.Utilities.Task
+	{
+		public override bool Execute()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
