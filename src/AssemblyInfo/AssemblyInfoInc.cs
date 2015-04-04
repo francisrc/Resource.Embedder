45718c687123c32bab87bde9bddca6e33e4ce87c
MarcStan <marcstan@live.com>
2015-04-04 20:48:21 +0200

    updated assemblyinfo

--- src/AssemblyInfo/AssemblyInfoInc.cs
+++ src/AssemblyInfo/AssemblyInfoInc.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.9.0.0")]
 [assembly: AssemblyFileVersion("0.9.0.0")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Marc Stanglmayr")]
 [assembly: AssemblyCopyright("Copyright © Marc Stanglmayr 2015")]
