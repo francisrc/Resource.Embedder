8d23ff1f3284f7031a0a0a8ab18bbb6b695a3453
MarcStan <marcstan@live.com>
2015-04-04 16:56:39 +0200

    added new test for fully testing embedding

---
+++ src/modules/ResourceEmbedder.Core/ResourceEmbedder.Core.Tests/FullEmbeddingTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace ResourceEmbedder.Core.Tests
+{
+	[TestFixture]
+	public class FullEmbeddingTests
+	{
+		#region Methods
+
+		[Test]
+		public void TestEmbedResourceAndInjectCode()
+		{
+			const string file = "WpfFullTest.exe";
+			if (File.Exists(file))
+			{
+				File.Delete(file);
+			}
+			File.Copy("WpfTest.exe", file);
+
+			var logger = Substitute.For<ILogger>();
+			IEmbedFiles embedder = new CecilBasedEmbedder(logger);
+			var resources = new[]
+			{
+				new ResourceInfo("de\\WpfTest.resources.dll", "WpfTest.de.resources.dll"),
+				new ResourceInfo("fr\\WpfTest.resources.dll", "WpfTest.fr.resources.dll")
+			};
+			embedder.EmbedResources(file, file, resources).Should().BeTrue();
+
+			IInjectCode injector = new CecilBasedCodeInjector(logger);
+			injector.Inject(file, CecilHelpers.InjectEmbeddedResourceLoader).Should().BeTrue();
+
+			throw new NotImplementedException("TODO: finish");
+			File.Delete(file);
+		}
+
+		#endregion Methods
+	}
+}
--- src/modules/ResourceEmbedder.Core/ResourceEmbedder.Core.Tests/InjectCodeTests.cs
+++ src/modules/ResourceEmbedder.Core/ResourceEmbedder.Core.Tests/InjectCodeTests.cs
@@ -22,17 +22,18 @@ namespace ResourceEmbedder.Core.Tests
 			}
 			File.Copy("WpfTest.exe", file);
 
+			// inject the localization assembly loader hooks
 			IInjectCode injector = new CecilBasedCodeInjector(Substitute.For<ILogger>());
 			injector.Inject(file, CecilHelpers.InjectEmbeddedResourceLoader).Should().BeTrue();
 
-			// now check that assembly has embedded code by using reflection
-
+			// now check that assembly has actually embedded that code by using reflection to access it
 			var asm = Assembly.LoadFrom(file);
 			var t = asm.Types().FirstOrDefault(t2 => t2.Name == "ResourceEmbedderILInjected");
 			t.Should().NotBeNull();
 
 			var methods = t.GetMethods(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
 			methods.Should().HaveCount(5);
+			// currently the class we inject uses these 5 methods
 			methods.Select(m => m.Name).Should().Contain(new[]
 			{
 				"FindMainAssembly",
